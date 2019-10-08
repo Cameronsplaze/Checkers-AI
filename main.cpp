ce4c820163d2cd76793585263fc1cfd095700030
Cameron <cjshowalter@alaska.edu>
2019-10-07 22:31:12 -0800

    added cxxopts.hpp for cli processing

--- main.cpp
+++ main.cpp
@@ -7,14 +7,33 @@
 #include "include/globalconsts.hpp"
 #include "include/checkerboard.hpp"
 #include "include/managerSFML.hpp"
+#include "include/cxxopts.hpp"
 
 #include "include/negamax.hpp"
 #include "include/catch.hpp"
 // #include "include/tests.hpp" // make tests.cpp too to reduce compile time.
 
 
-int main(int argc, char const *argv[])
+// this auto'd to: int main(int argc, char const *argv[])
+// BUT cxxopts parse doesn't take a "const". Look into if this is bad.
+int main(int argc, char *argv[])
 {    
+
+    // Parser used for command line args: https://github.com/jarro2783/cxxopts
+    cxxopts::Options options("Checkers AI", "One AI to rule them all");
+    options.add_options()
+        ("h,help", "Prints help... maybe")
+        ("f,file", "File name", cxxopts::value<std::string>())
+        ;
+    auto result = options.parse(argc, argv);
+    // std::cout << (std::string) result << std::endl;
+    if(result.count("help") > 0)
+    {
+        std::cout << "IT WORKS!!!!" << std::endl;
+        return 0;
+    }
+    std::cout << boardToString(getStartBoard()) << std::endl;
+    return 0;
 
     				//	01234567890123456789012345678901
     // std::string tmp1 = "b___r____BR______Rbbr____Br_____";
@@ -23,16 +42,20 @@ int main(int argc, char const *argv[])
     // std::bitset<96> tmpB1 = stringToBoard(tmp1);
     // std::bitset<96> tmpB2 = stringToBoard(tmp2);
     // std::cout << tmpB2 << " Start" << std::endl; 
-    CheckerboardGUI("random", "piece_count");
 
-    // std::vector<std::bitset<96>> possBoards = std::move( CheckerBoardManager(tmpB1, redTurn, false).getAllMoves() );
+    // ######################
+    CheckerboardGUI("random", "random");
+    // CheckerboardGUI("random", "piece_count");
+    // ######################
+
+    // std::vector<std::bitset<96>> possBoards = std::move( CheckerBoardMoves(tmpB1, redTurn, false).getAllMoves() );
     // for (int i=0; i<possBoards.size(); ++i)
     // {
     //    std::cout << boardToString(possBoards[i]) << " " << i << std::endl;
     // }
 
 
-    if(false)
+    if(true)
     {
         Catch::Session session; // There must be exactly one instance
 
